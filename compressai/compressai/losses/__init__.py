b3631787450984f4b3af01bcf6fe3226a59e4226
oguzhangungordu <45096462+oguzhangungordu@users.noreply.github.com>
2023-04-03 02:51:40 +0300

    Delete compressai/compressai directory

--- compressai/compressai/losses/__init__.py
+++
@@ -1,7 +0,0 @@
-from .rate_distortion import RateDistortionLoss
-from .rate_distortion_saliency import RateDistortionLossSaliency
-
-__all__ = [
-    "RateDistortionLoss",
-    "RateDistortionLossSaliency",
-]
