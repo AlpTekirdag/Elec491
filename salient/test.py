a7101c5033e2bd8b8cdfdf4bacfbdccdf40bf417
AlpTekirdag <atekirdag17@ku.edu.tr>
2023-05-05 16:31:52 +0300

    salient test

--- salient/test.py
+++ salient/test.py
@@ -16,6 +16,54 @@ from attention import Sal_based_Attention_module
 from salgan import SalGAN_Generator
 
 
+model = SalGAN_2()
+
+weight = torch.load('weight/ckpt_epoch_71.pth')
+
+model.load_state_dict(weight['model'], strict=True)
+
+model.cuda()
+
+IMG_Path = "Eval/normal/test/"
+
+image_list = os.listdir(IMG_Path)
+
+
+"""if not os.path.isdir('./V'):
+    os.makedirs('./V')"""
+
+if not os.path.isdir('./result'):
+    os.makedirs('./result')
+
+
+print(image_list)
+# i =80
+for img in image_list:
+    image_path = IMG_Path + img
+    print(img)
+    ori = cv2.imread(image_path)
+    #inpt = cv2.resize(ori,(320, 160))
+    inpt = ori 
+    inpt = np.float32(inpt)
+    #inpt-=[0.485, 0.456, 0.406]
+    inpt = torch.cuda.FloatTensor(inpt)
+
+    inpt = inpt.permute(2, 0, 1)
+    
+    inpt = torch.cuda.FloatTensor(inpt)
+
+    with torch.no_grad():
+        saliency_map = model(inpt.unsqueeze(0))
+
+    Output = saliency_map
+    Output = (Output.cpu()).detach().numpy()
+    Output = Output.squeeze()
+    Output = resize(Output, (1024,2048))
+    #np.save('./V/'+img[:-4]+'.npy',Output)
+    cv2.imwrite('./result/'+img[:-4]+'.png',(Output-Output.min())*255/(Output.max()-Output.min()))
+
+
+"""
 def normalize(x, method='standard', axis=None):
     x = np.array(x, copy=False)
     if axis is not None:
@@ -49,76 +97,4 @@ def CC(saliency_map1, saliency_map2):
 	map1 = normalize(map1, method='standard')
 	map2 = normalize(map2, method='standard')
 	return np.corrcoef(map1.ravel(), map2.ravel())[0,1]
-
-
-
-#Contrastive
-
-model = SalGAN_4()
-
-weight = torch.load('./models/model_encoder:_resnet_pool_decoder:_rand_lr:_0.0001_bz:_64_opt:_Adam/ckpt_epoch_56.pth')
-#weight = torch.load('./trained_models/random_decoder/final_models/finetuned.pth')
-
-model.load_state_dict(weight['model'], strict=True)
-
-
-"""
-#Salgan
-model = SalGAN_Generator()
-weight = torch.load('./salgan_3dconv_module.pt')
-
-model.load_state_dict(weight, strict=False)
-
-"""
-
-"""
-#Attention
-model = Sal_based_Attention_module()
-
-weight = torch.load('./attention.pt')
-model.load_state_dict(weight['state_dict'], strict=False)
-
-"""
-
-
-model.cuda()
-
-
-IMG_Path = "/home/yasser/Desktop/DATA360/images/"
-
-
-image_list = os.listdir(IMG_Path)
-
-
-if not os.path.isdir('./V'):
-    os.makedirs('./V')
-
-if not os.path.isdir('./saliency'):
-    os.makedirs('./saliency')
-
-
-print(image_list)
-# i =80
-for img in image_list:
-    image_path = IMG_Path + img
-    print(img)
-    ori = cv2.imread(image_path)
-    inpt = cv2.resize(ori,(320, 160))
-
-    inpt = np.float32(inpt)
-    #inpt-=[0.485, 0.456, 0.406]
-    inpt = torch.cuda.FloatTensor(inpt)
-
-    inpt = inpt.permute(2, 0, 1)
-    
-    inpt = torch.cuda.FloatTensor(inpt)
-
-    with torch.no_grad():
-        saliency_map = model(inpt.unsqueeze(0))
-
-    Output = saliency_map
-    Output = (Output.cpu()).detach().numpy()
-    Output = Output.squeeze()
-    Output = resize(Output, (1024,2048))
-    np.save('./V/'+img[:-4]+'.npy',Output)
-    cv2.imwrite('./saliency/'+img[:-4]+'.png',(Output-Output.min())*255/(Output.max()-Output.min()))
+    """
